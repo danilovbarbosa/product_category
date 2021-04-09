6524f42914f30976a2846660a193121dc5969c97
Eduardo Pinheiro <edufpin@gmail.com>
2021-04-09 16:28:23 -0300

    refactor(category): adicionado tratamento de exceção

--- category.py
+++ category.py
@@ -2,29 +2,36 @@ from models import Category
 from sqlalchemy import create_engine
 engine = create_engine('sqlite:///shop.db', echo = True)
 from sqlalchemy.orm import sessionmaker
-
+Session = sessionmaker(bind = engine)
 
 def create_category():
     name = input('Informe um nome para a sua categoria: ')
     description = input('Informe uma descrição para esta categoria: ')
     new_category = Category(name = name, description = description)
-    Session = sessionmaker(bind = engine)
-    session = Session()
-    session.add(new_category)
-    session.commit()
+    data_category(new_category)
 
+def data_category(category):
+    try:
+        session = Session()
+        session.add(category)
+        session.commit()
+    except:
+        session.rollback()
+        raise Exception("Erro, ao adicionar categoria")
+    finally:
+        session.close()
 
-def data_category(category_object):
-    new_category = str(category_object) + '\n'
-    data = open('categories.txt', 'a')
-    data.write(new_category)
-    print('ok')
 
 
 def list_of_category():
-    print('\nCATEGORIAS:\n')
-    Session = sessionmaker(bind = engine)
-    session = Session()
-    all_categories = session.query(Category).all()
-    for row in all_categories:
-        print ("Nome:",row.name, "Descrição:",row.description)
+    try:
+        session = Session()
+        all_categories = session.query(Category).all()
+        print('\nCATEGORIAS:\n')
+        for row in all_categories:
+            print ("Nome:",row.name, "Descrição:",row.description)
+    except:
+        raise Exception("Erro, ao adicionar categoria")
+    finally:
+        session.close()
+
