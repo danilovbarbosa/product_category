fb2326abd5806687286dfe28f938cc6793722206
danilovbarbosa <danilo.v.barbosa@gmail.com>
2021-04-09 14:42:05 -0300

    feat(models): definidas models para categoria e produto usando o ORM SQLAlchemy.

---
+++ models.py
@@ -0,0 +1,31 @@
+from sqlalchemy import create_engine, ForeignKey, Column, Integer, String, Float
+engine = create_engine('sqlite:///shop.db', echo = True)
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+from sqlalchemy.orm import relationship
+
+
+class CategoryProduct(Base):
+    __tablename__ = 'category_product'
+
+    id_category = Column(Integer, ForeignKey('category.id'), primary_key = True)
+    id_product = Column(Integer, ForeignKey('product.id'), primary_key = True)
+
+
+class Category(Base):
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key = True)
+    name = Column(String)
+    description = Column(String)
+
+
+class Product(Base):
+    __tablename__ = 'product'
+    id = Column(Integer, primary_key = True)
+    name = Column(String)
+    value = Column(Float)
+    description = Column(String)
+    categories = relationship('Category', secondary = 'category_product')
+
+
+Base.metadata.create_all(engine)
