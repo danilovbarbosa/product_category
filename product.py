d8f5505ab611e0d21b7e49dcf600935add04a21a
danilovbarbosa <danilo.v.barbosa@gmail.com>
2021-04-09 15:18:38 -0300

    refactor(product): início da refatoração das funções create_product e data_product.

--- product.py
+++ product.py
@@ -1,27 +1,47 @@
 from category import list_of_category
+from models import Product
+from sqlalchemy.orm import sessionmaker
 
 
 def create_product():
+    '''
+        User Story: Como usuário, eu quero criar um produto para tê-lo registrado.
+        Desciption: Deve ler name, description, value, category; criar um produto e registrar no BD.
+
+    '''
+
     list_of_category()
-    print()
+
     name = input('Digite o nome do produto: ')
     description = input('Digite a descrição do produto: ')
     value = float(input('Digite o valor do produto R$ '))
     category = int(input('Digite o código da categoria: '))
-    register_product = f'{name},{description},{value},{category}'
-    data_product(register_product)
+
+    data_product(name, description, value, category)
 
 
-def data_product(product_object):
-    new_product = str(product_object) + '\n'
-    data = open('product.txt', 'a')
-    data.write(new_product)
-    print('ok')
+def data_product(name, description, value, category):
+    '''
+        TODO: refatorar
+    '''
+    '''
+        Desciption: Deve receber um projeto e salvar no BD.
+    '''
+    register_product = Product(name=name, value=value, description=description)
+    product_object.categories.append()
 
 
 def list_of_product():
+    '''
+        TODO: refatorar
+    '''
+    '''
+        Desciption: Deve receber um projeto e salvar no BD.
+    '''
     list_products = open('product.txt', 'r')
+    
     print('\nPRODUTOS:\n')
+    
     for product in list_products:
         items_product = product.split(',')
         print(f'Nome: {items_product[0]}\nDescrição: {items_product[1]}\n Valor: {items_product[2]}\nCódigo da categoria: {items_product[3]}')
